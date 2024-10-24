003b674a327cba8083001911d0d517bb74a5ea89
Nitaa1904 <nitafitrotul1904@gmail.com>
2024-10-25 01:31:50 +0700

    make dynamic query API get all data users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,50 @@
 const { Users } = require("../models");
+const { Op } = require("sequelize");
 
 const findUsers = async (req, res, next) => {
   try {
-    const users = await Users.findAll();
+    // Ambil query params untuk filtering
+    const { name, minAge, maxAge, role, address } = req.query;
+
+    const userCondition = {};
+
+    if (name) userCondition.name = { [Op.iLike]: `%${name}%` };
+    if (minAge) userCondition.age = { [Op.gte]: minAge };
+    if (maxAge) {
+      userCondition.age = {
+        ...userCondition.age,
+        [Op.lte]: maxAge,
+      };
+    }
+    if (role) userCondition.role = { [Op.iLike]: `%${role}%` };
+    if (address) userCondition.address = { [Op.iLike]: `%${address}%` };
+
+    // Fetch semua pengguna berdasarkan syarat pencarian
+    const users = await Users.findAll({
+      where: userCondition,
+      attributes: ["id", "name", "age", "role", "address"],
+    });
+
+    const totalData = users.length;
 
     res.status(200).json({
       status: "Success",
+      message: "Berhasil mendapatkan data pengguna",
+      isSuccess: true,
       data: {
+        totalData,
         users,
       },
     });
-  } catch (err) {}
+  } catch (err) {
+    console.error(err.name);
+    res.status(500).json({
+      status: "Failed",
+      message: "Terjadi kesalahan pada server",
+      isSuccess: false,
+      data: null,
+    });
+  }
 };
 
 const findUserById = async (req, res, next) => {
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,7 @@ const authController = require("../controllers/authController");
 // const shopControlle = require("../controllers/userControlle");
 
 // router.post("", shopController.createShop);
-// router.get("", userController.findUsers);
+router.get("", userController.findUsers);
 // router.get("/:id", shopController.getShopById);
 // router.patch("/:id", shopController.updateShop);
 // router.delete("/:id", shopController.deleteShop);
